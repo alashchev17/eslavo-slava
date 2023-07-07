4f838ec795df7739859e875a24b1f62f433481a8
alashchev17 <andrew.lashchev15@gmail.com>
2023-07-08 01:30:28 +0200

    Pre-commit (The Project, Line Up)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-const heroSlide = document.querySelector(".animation__slide");
+const heroSlide = document.querySelector(".animation__slide"),
+      lineUpSlide = document.querySelector(".line-up__slide");
 const preloader = document.querySelector(".preloader");
 const preloaderInfo = document.querySelector(".preloader__info");
 
@@ -46,8 +47,36 @@ window.onload = () => {
         document.body.style.overflow = "auto";
         setTimeout(() => {
           slidesHandler(heroSlide, 1, 6);
+          slidesHandler(lineUpSlide, 1, 5);
         }, 1000);
       }, 150);
     }, 1500);
   }, 1500);
 }
+
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    const blockID = anchor.getAttribute('href');
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    });
+  });
+}
+
+const lineUpCards = document.querySelectorAll(".line-up__card");
+const lineUpTriangles = document.querySelectorAll(".line-up__card-triangle");
+const lineUpDescs = document.querySelectorAll(".line-up__card-desc");
+
+lineUpCards.forEach((item, index) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
+    lineUpTriangles[index].classList.toggle("active");
+    lineUpDescs[index].classList.toggle("hidden");
+  });
+});
